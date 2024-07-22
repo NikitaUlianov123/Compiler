5f3fe8ea6b5220861fd05b6e6f2cff8773ca2144
Nikita Ulianov <nikita@mectruck.com>
2024-07-22 10:29:38 -0700

    Renamed solution to match repo

--- FishCompiler/Program.cs
+++ FishCompiler/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace FishCompiler
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace FishCompiler
 {
     public enum Classification
     { 
@@ -14,7 +18,7 @@
         parser,
         conditional
     };
-    
+
     internal class Program
     {
         static void Main(string[] args)
@@ -31,6 +35,46 @@
             var tree = Parser.Parse(tokens, ref number);
 
             var analysis = Semantic.Analyse(tree);
+
+
+
+            var programName = "runme";
+            var filename = $"{programName}.exe";
+
+            var namespaceName = "Code";
+            var className = "Program";
+            var moduleName = $"{namespaceName}.{className}";
+            var entryPointMethodName = "Main";
+
+            var assemblyName = new AssemblyName("Program");
+            var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName,
+                                                        AssemblyBuilderAccess.Save);
+
+            var moduleBuilder = assemblyBuilder.DefineDynamicModule(moduleName, filename);
+
+            var programClassBuilder = moduleBuilder.DefineType("Program",
+                                        TypeAttributes.Public | TypeAttributes.Class);
+
+            var mainMethodBuilder = programClassBuilder.DefineMethod(name: entryPointMethodName,
+                                        attributes: MethodAttributes.Public | MethodAttributes.Static,
+                                        returnType: typeof(void),
+                                        parameterTypes: new[] { typeof(string[]) });
+
+            var mainILGen = mainMethodBuilder.GetILGenerator();
+
+
+
+            mainILGen.Emit(OpCodes.Ldstr, "Hello, World!");
+            mainILGen.Emit(OpCodes.Call, typeof(Console).GetMethod("WriteLine", new[] { typeof(string) }));
+            mainILGen.Emit(OpCodes.Ret);
+
+
+
+            var programClass = programClassBuilder.CreateType();
+
+            assemblyBuilder.SetEntryPoint(mainMethodBuilder, PEFileKinds.ConsoleApplication);
+
+            assemblyBuilder.Save(filename);
         }
     }
 }
